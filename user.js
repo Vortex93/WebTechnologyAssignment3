d1f267c1a8c799d24254415a0afccda4cebc6699
e_voe_000 <e_voetbal_guy@hotmail.com>
2016-09-28 15:46:07 +0200

    add getters in user

--- user.js
+++ user.js
@@ -1,7 +1,29 @@
-module.exports = function (lastName, middleName, firstName, userName, password) {
+var User = function (lastName, middleName, firstName, userName, password) {
     this.lastName = lastName;
     this.middleName = middleName;
     this.firstname = firstName;
     this.userName = userName;
     this.password = password;
-}
+};
+
+User.prototype.getLastName =  function () {
+    return this.lastName;
+};
+
+User.prototype.getMiddleName = function () {
+    return this.middleName;
+};
+
+User.prototype.getfirstName = function () {
+    return this.firstname;
+};
+
+User.prototype.getUserName = function () {
+    return this.userName;
+};
+
+User.prototype.getPassword = function () {
+    return this.password;
+};
+
+module.exports = User;
