f857ef5de6193cdf91d4c6232309de0eee041b16
e_voe_000 <e_voetbal_guy@hotmail.com>
2016-09-28 15:15:08 +0200

    add: user and rating class

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
 var Express = require("express");
 
 var express = new Express();
+
+var User = function (lastName, middleName, firstName, userName, password) {
+    this.lastName = lastName;
+    this.middleName = middleName;
+    this.firstname = firstName;
+    this.userName = userName;
+    this.password = password;
+}
+
+var Rating = function (movie, user, stars) {
+    this.movie = movie;
+    this.user = user;
+    this.stars = stars;
+}
